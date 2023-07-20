1f4240cace27efe3e9be540a3106667c2fbb2cdf
Hellaeh <me@hellaeh.com>
2023-07-20 18:47:27 +0300

    Added ignored output test

---
+++ tests/output.rs
@@ -0,0 +1,11 @@
+#![allow(clippy::assertions_on_constants)]
+
+use hel_time::iso8601utc;
+
+#[test]
+#[ignore = "to see output run 'rust t -- --ignored'"]
+fn output_iso8601utc() {
+	println!("{}", iso8601utc());
+
+	assert!(false);
+}
